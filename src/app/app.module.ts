86e98e88bc685508b07648c52b9bebab001834fd
Anna <sancho1234576@gmail.com>
2018-10-09 21:21:30 +0300

    generate statistics component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { MapComponent } from './map/map.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { StatisticsComponent } from './sidebar/statistics/statistics.component';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent, SidebarComponent],
+  declarations: [AppComponent, MapComponent, SidebarComponent, StatisticsComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
