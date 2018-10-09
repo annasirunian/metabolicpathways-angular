f69b33ca158758d01c05628c12fca6841a6d922e
Anna <sancho1234576@gmail.com>
2018-10-09 15:16:41 +0300

    add statistics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Builder } from "escher-vis";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  nodeTypes = [];
+  genes = [];
+
   ngOnInit() {
     this.embedPathwayMap(null);
     document
@@ -30,6 +33,7 @@ export class AppComponent {
     try {
       let data = JSON.parse(text);
       this.embedPathwayMap(data);
+      this.getStatistics(data);
       document.getElementById("load_error").className = "";
     } catch (err) {
       console.error("Unable to load file: ", err);
@@ -56,5 +60,57 @@ export class AppComponent {
     client.open("GET", "demo.json");
     client.onload = () => this.loadFile(client.responseText);
     client.send();
+  }
+
+  clearStatistics() {
+    let container = document.getElementById("statistics_container");
+    if (container.firstChild.nodeName === "SPAN") {
+      container.removeChild(container.firstChild);
+    }
+    this.nodeTypes = [];
+    this.genes = [];
+  }
+
+  getStatistics(data) {
+    this.clearStatistics();
+    document.getElementById("node_statistics").className = "visible";
+    document.getElementById("gene_statistics").className = "visible";
+
+    // Nodes statistics
+    let nodeTypes = {};
+    let nodes = Object.values(data[1].nodes);
+    for (let node of nodes) {
+      let nodeType = node.node_type;
+      if (!(nodeType in nodeTypes)) {
+        nodeTypes[nodeType] = 0;
+      }
+      nodeTypes[nodeType]++;
+    }
+    for (let type of Object.keys(nodeTypes)) {
+      this.nodeTypes.push({ type: type, number: nodeTypes[type] });
+    }
+
+    // Genes statistics
+    let genes = {};
+    let reactions = Object.values(data[1].reactions);
+    for (let reaction of reactions) {
+      for (let geneInReaction of reaction.genes) {
+        let gene = geneInReaction.name;
+        if (!(gene in genes)) {
+          genes[gene] = 0;
+        }
+        genes[gene]++;
+      }
+    }
+
+    let geneNames = Object.keys(genes);
+    for (let name of geneNames) {
+      if (genes[name] <= 1) {
+        delete genes[name];
+      }
+    }
+    for (let gene of Object.keys(genes)) {
+      this.genes.push({ name: gene, reactions: genes[gene] });
+    }
   }
 }
