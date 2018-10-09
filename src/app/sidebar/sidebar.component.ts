c6c1869817336cca577bac36f88b527288ec3eb1
Anna <sancho1234576@gmail.com>
2018-10-10 00:04:39 +0300

    extract functionality into sidebar and statistics components

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { StatisticsComponent } from "./statistics/statistics.component";
+import { Component, EventEmitter, Output, ViewChild } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.css"]
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
+  @Output()
+  embedPathwayMapClicked = new EventEmitter();
 
-  constructor() { }
+  @ViewChild(StatisticsComponent)
+  statisticsComponent: StatisticsComponent;
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {}
+
+  isErrorVisible = false;
+  theme = "default_theme";
+
+  embedPathwayMap(data) {
+    this.embedPathwayMapClicked.emit(data);
   }
 
+  loadFile(text, toParse) {
+    try {
+      let data;
+      if (toParse) {
+        data = JSON.parse(text);
+      } else {
+        data = text;
+      }
+      this.embedPathwayMap(data);
+      this.statisticsComponent.getStatistics(data);
+      this.isErrorVisible = false;
+    } catch (err) {
+      console.error("Unable to load file: ", err);
+      this.isErrorVisible = true;
+    }
+  }
+
+  handleThemeChange(evt) {
+    this.theme = evt.target.value;
+  }
+
+  handleFileSelect(evt) {
+    let file = evt.target.files[0];
+    let reader = new FileReader();
+    reader.onload = e => this.loadFile(e.target.result, true);
+    reader.readAsText(file);
+    document.getElementById("file_selector").value = "";
+  }
+
+  handleShowDemo() {
+    this.http.get("demo.json").subscribe(data => this.loadFile(data, false));
+  }
 }
--- src/app/sidebar/statistics/statistics.component.ts
+++ src/app/sidebar/statistics/statistics.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-statistics',
-  templateUrl: './statistics.component.html',
-  styleUrls: ['./statistics.component.css']
+  selector: "app-statistics",
+  templateUrl: "./statistics.component.html",
+  styleUrls: ["./statistics.component.css"]
 })
-export class StatisticsComponent implements OnInit {
+export class StatisticsComponent {
+  areTablesVisible = false;
+  nodeTypes = [];
+  genes = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  clearStatistics() {
+    this.areTablesVisible = true;
+    this.nodeTypes = [];
+    this.genes = [];
   }
 
+  getStatistics(data) {
+    this.clearStatistics();
+
+    // Nodes statistics
+    let nodeTypes = {};
+    let nodes = Object.values(data[1].nodes);
+    for (let node of nodes) {
+      let nodeType = node.node_type;
+      if (!(nodeType in nodeTypes)) {
+        nodeTypes[nodeType] = 0;
+      }
+      nodeTypes[nodeType]++;
+    }
+    for (let type of Object.keys(nodeTypes)) {
+      this.nodeTypes.push({ type: type, number: nodeTypes[type] });
+    }
+
+    // Genes statistics
+    let genes = {};
+    let reactions = Object.values(data[1].reactions);
+    for (let reaction of reactions) {
+      for (let geneInReaction of reaction.genes) {
+        let gene = geneInReaction.name;
+        if (!(gene in genes)) {
+          genes[gene] = 0;
+        }
+        genes[gene]++;
+      }
+    }
+
+    let geneNames = Object.keys(genes);
+    for (let name of geneNames) {
+      if (genes[name] <= 1) {
+        delete genes[name];
+      }
+    }
+    for (let gene of Object.keys(genes)) {
+      this.genes.push({ name: gene, reactions: genes[gene] });
+    }
+  }
 }
