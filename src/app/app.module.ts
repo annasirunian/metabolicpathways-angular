3d967d07b21ff59b0b120188c9558bdbecb6051c
Anna <sancho1234576@gmail.com>
2018-10-09 21:13:23 +0300

    generate sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { MapComponent } from './map/map.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent],
+  declarations: [AppComponent, MapComponent, SidebarComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
